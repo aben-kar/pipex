6af720cdcad0b93d1964c46d90c25bdf93ef6641
Achraf Ben Kardoud <acben-ka@e1r5p8.1337.ma>
2025-01-28 18:50:32 +0100

    tt2

--- pipex.c
+++ pipex.c
@@ -1,25 +1,30 @@
 #include "pipex.h"
 
 int main(int ac, char **av, char **envp) {
-    if (ac > 2) {
-        char **split_av = ft_split(av[1], ' ');
-        char **split_av1 = ft_split(av[2], ' ');
+    if (ac > 3) {
+        char **split_av = ft_split(av[2], ' ');
+        char **split_av1 = ft_split(av[3], ' ');
         if (!split_av || !split_av1) {
             return 1;
         }
 
         int fd[2];
+            //fd[0] ==> read;
+            //fd[1] == write;
         if (pipe(fd) == -1) {
             return 1;
         }
 
-        int id = fork();
-        if (id == 0) {
+        int id1 = fork();
+        if (id1 == 0) {
+            int in_file = open(av[1], O_RDONLY | O_CREAT, 0644);
             close(fd[0]);
             dup2(fd[1], STDOUT_FILENO);
             close(fd[1]);
+            dup2(in_file, 0);
+            close(in_file);
 
-            if (!(access(split_av[0], F_OK | X_OK)))
+            if ((access(split_av[0], F_OK | X_OK)) == 0)
                 execve(split_av[0], split_av, envp);
 
             char **res = envp;
@@ -32,7 +37,7 @@ int main(int ac, char **av, char **envp) {
                         while (split_path[i]) {
                             char *tmp = ft_strjoin(split_path[i], "/");
                             char *cmd = ft_strjoin(tmp, split_av[0]);
-                            if (!(access(cmd, F_OK | X_OK))) {
+                            if ((access(cmd, F_OK | X_OK)) == 0) {
                                 execve(cmd, split_av, envp);
                             }
                             i++;
@@ -42,36 +47,49 @@ int main(int ac, char **av, char **envp) {
                 res++;
             }
         } else {
-            close(fd[1]);
-            dup2(fd[0], STDIN_FILENO);
-            close(fd[0]);
+            
+            int id2 = fork();
+            if (id2 == 0) {
+                int output_file = open(av[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                close(fd[1]);
+                dup2(fd[0], 0);
+                close(fd[0]);
+                dup2(output_file, STDOUT_FILENO);
+                close(output_file);
 
-            if (!(access(split_av1[0], F_OK | X_OK)))
-                execve(split_av1[0], split_av1, envp);
-
-            char **res1 = envp;
-            while (*res1) {
-                char *found1 = ft_strnstr(*res1, "PATH=", 5);
-                if (found1) {
-                    char **split_path1 = ft_split(found1 + 5, ':');
-                    if (split_path1) {
-                        int j = 0;
-                        while (split_path1[j]) {
-                            char *tmp1 = ft_strjoin(split_path1[j], "/");
-                            char *cmd1 = ft_strjoin(tmp1, split_av1[0]);
-                            if (!(access(cmd1, F_OK | X_OK))) {
-                                execve(cmd1, split_av1, envp);
+                if ((access(split_av1[0], F_OK | X_OK)) == 0)
+                {
+                     execve(split_av1[0], split_av1, envp);
+                }
+               
+                char **res1 = envp;
+                while (*res1) {
+                    char *found1 = ft_strnstr(*res1, "PATH=", 5);
+                    if (found1) {
+                        char **split_path1 = ft_split(found1 + 5, ':');
+                        if (split_path1) {
+                            int j = 0;
+                            while (split_path1[j]) {
+                                char *tmp1 = ft_strjoin(split_path1[j], "/");
+                                char *cmd1 = ft_strjoin(tmp1, split_av1[0]);
+                                if ((access(cmd1, F_OK | X_OK)) == 0) {
+                                    execve(cmd1, split_av1, envp);
+                                }
+                                j++;
                             }
-                            j++;
                         }
                     }
+                    res1++;
                 }
-                res1++;
             }
+            close(fd[0]);
+            close(fd[1]);
+            wait(NULL);
             wait(NULL);
         }
     } else {
-        write(STDERR_FILENO, "Usage: ./pipex \"cmd1\" \"cmd2\"\n", 30);
+        return (-1);
     }
     return 0;
 }
+
--- test1.c
+++ test1.c
@@ -1,115 +1,79 @@
 #include "pipex.h"
 
-int main(int ac, char **av, char **envp)
-{
-    if(ac > 0)
-    {
+int main(int ac, char **av, char **envp) {
+    if (ac > 2) {
         char **split_av = ft_split(av[1], ' ');
         char **split_av1 = ft_split(av[2], ' ');
-        if(!split_av || !split_av1)
-            return 0;
-        // ft_free(split_av);
-        // ft_free(split_av1);
-
-        int fd[2];
-        if (pipe(fd) == -1)
-        {
-            ft_free(split_av);
-            ft_free(split_av1);
+        if (!split_av || !split_av1) {
             return 1;
         }
-    
-        int id = fork();
 
-        if (id == 0)
-        {
+        int fd[2];
+        if (pipe(fd) == -1) {
+            return 1;
+        }
+
+        int id = fork();
+        if (id == 0) {
             close(fd[0]);
             dup2(fd[1], STDOUT_FILENO);
             close(fd[1]);
 
-            if (!(access(split_av[0], F_OK | X_OK)))
-                execve(av[1], split_av, envp);
-        
+            if ((access(split_av[0], F_OK | X_OK)) == 0)
+                execve(split_av[0], split_av, envp);
 
             char **res = envp;
-            while(*res)
-            {
+            while (*res) {
                 char *found = ft_strnstr(*res, "PATH=", 5);
-                if (found)
-                {
+                if (found) {
                     char **split_path = ft_split(found + 5, ':');
-                    if(split_path)
-                    {
+                    if (split_path) {
                         int i = 0;
-                        while(split_path[i])
-                        {
+                        while (split_path[i]) {
                             char *tmp = ft_strjoin(split_path[i], "/");
                             char *cmd = ft_strjoin(tmp, split_av[0]);
-                            free(tmp);
-                            if (!(access(cmd, F_OK | X_OK)))
-                            {
-                                free(cmd);
-                                ft_free(split_path);
-                                ft_free(split_av);
-                                ft_free(split_av1);
+                            if ((access(cmd, F_OK | X_OK)) == 0) {
                                 execve(cmd, split_av, envp);
                             }
-                            free(cmd);
                             i++;
                         }
-                        ft_free(split_path);
                     }
                 }
                 res++;
             }
-            ft_free(split_av);
-            ft_free(split_av1);
-        }
-        else
-        {
+        } else {
             close(fd[1]);
             dup2(fd[0], STDIN_FILENO);
             close(fd[0]);
 
-            if (!(access(split_av1[0], F_OK | X_OK)))
-                execve(av[2], split_av1, envp);
+            if ((access(split_av1[0], F_OK | X_OK)) == 0)
+                execve(split_av1[0], split_av1, envp);
 
             char **res1 = envp;
-            while(*res1)
-            {
+            while (*res1) {
                 char *found1 = ft_strnstr(*res1, "PATH=", 5);
-                if(found1)
-                {
-                    char **split_path1 = ft_split(found1, ':');
-                    if (split_path1)
-                    {
+                if (found1) {
+                    char **split_path1 = ft_split(found1 + 5, ':');
+                    if (split_path1) {
                         int j = 0;
-                        while (split_path1[j])
-                        {
+                        while (split_path1[j]) {
                             char *tmp1 = ft_strjoin(split_path1[j], "/");
                             char *cmd1 = ft_strjoin(tmp1, split_av1[0]);
-                            free(tmp1);
-                            if (!(access(cmd1, F_OK | X_OK)))
-                            {
-                                free(cmd1);
-                                ft_free(split_path1);
-                                ft_free(split_av);
-                                ft_free(split_av1);
+                            if ((access(cmd1, F_OK | X_OK)) == 0) {
                                 execve(cmd1, split_av1, envp);
                             }
-                            free(cmd1);
                             j++;
                         }
-                        ft_free(split_path1);
                     }
                 }
                 res1++;
             }
-            ft_free(split_av);
-            ft_free(split_av1);
             wait(NULL);
         }
+    } else {
+        write(STDERR_FILENO, "Usage: ./pipex \"cmd1\" \"cmd2\"\n", 30);
     }
+    return 0;
 }
 
 
