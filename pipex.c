2e7af39786ba8d8074e7e077959d920e442b57ee
your name <you@example.com>
2025-01-31 17:33:34 +0100

    try

--- pipex.c
+++ pipex.c
@@ -1,6 +1,6 @@
 #include "pipex.h"
 
-void execute_command(char **cmd, char **envp) {
+void execute_command(char **cmd, char **envp) { //31 line
     if ((access(cmd[0], F_OK | X_OK)) == 0)
         execve(cmd[0], cmd, envp);
 
@@ -14,15 +14,22 @@ void execute_command(char **cmd, char **envp) {
                 while (split_path[i]) {
                     char *tmp = ft_strjoin(split_path[i], "/");
                     char *cmd_path = ft_strjoin(tmp, cmd[0]);
-                    if ((access(cmd_path, F_OK | X_OK)) == 0)
+                    free(tmp);
+                    if ((access(cmd_path, F_OK | X_OK)) == 0) {
                         execve(cmd_path, cmd, envp);
+                        free(cmd_path);
+                        ft_free(split_path);
+                    }
+                    free(cmd_path);
                     i++;
                 }
             }
+            ft_free(split_path);
         }
         res++;
     }
     write(2, "command not found\n", 18);
+    exit(1);
 }
 
 void child_process_1(int *fd, char *input_file, char **cmd, char **envp) {
@@ -31,12 +38,13 @@ void child_process_1(int *fd, char *input_file, char **cmd, char **envp) {
         write(2, "Error opening input file\n", 26);
         exit(1);
     }
-    close(fd[0]);
-    dup2(fd[1], STDOUT_FILENO);
-    close(fd[1]);
     dup2(in_file, 0);
     close(in_file);
+    close(fd[0]);
+    dup2(fd[1], 1);
+    close(fd[1]);
     execute_command(cmd, envp);
+    ft_free(cmd);
 }
 
 void child_process_2(int *fd, char *output_file, char **cmd, char **envp) {
@@ -48,12 +56,13 @@ void child_process_2(int *fd, char *output_file, char **cmd, char **envp) {
     close(fd[1]);
     dup2(fd[0], 0);
     close(fd[0]);
-    dup2(out_file, STDOUT_FILENO);
+    dup2(out_file, 1);
     close(out_file);
     execute_command(cmd, envp);
+    ft_free(cmd);
 }
 
-int main(int ac, char **av, char **envp) {
+int main(int ac, char **av, char **envp) { //34 line
     if (ac != 5) {
         write(2, "Failed arguments!\n", 18);
         return 1;
@@ -83,5 +92,7 @@ int main(int ac, char **av, char **envp) {
     wait(NULL);
     wait(NULL);
     
+    ft_free(cmd1);
+    ft_free(cmd2);
     return 0;
 }
