528ae7cdec0561d8b33a900299b893ddf6cafee1
Achraf Ben Kardoud <acben-ka@e1r3p3.1337.ma>
2025-02-04 00:11:32 +0100

    ty

--- pipex.c
+++ pipex.c
@@ -4,15 +4,8 @@ char *find_executable_path(char *cmd, char **envp) {
     char **directories = NULL;
     char *cmd_path = NULL;
 
-    if (!cmd || !*cmd) {
-        write(2, "Error: Empty command\n", 21);
-        return NULL;
-    }
-
     if ((access(cmd, F_OK | X_OK)) == 0)
-        return (free(cmd), NULL);
-    
-
+        return (cmd); 
     directories = get_path_directories(envp);
     if (!directories)
         return NULL;
@@ -21,14 +14,12 @@ char *find_executable_path(char *cmd, char **envp) {
     while (directories[i]) {
         char *tmp = ft_strjoin(directories[i], "/");
         if (!tmp) {
-            ft_free(directories);
-            return NULL;
+            return ft_free(directories);
         }
         cmd_path = ft_strjoin(tmp, cmd);
         free(tmp);
         if (!cmd_path) {
-            ft_free(directories);
-            return NULL;
+            return ft_free(directories);
         }
         if ((access(cmd_path, F_OK | X_OK)) == 0) {
             ft_free(directories);
@@ -41,26 +32,26 @@ char *find_executable_path(char *cmd, char **envp) {
     return NULL;
 }
 
-void execute_command(char **cmd, char **envp) {
-    char *cmd_path = NULL;
-    if (!cmd || !cmd[0]) {
-        write(2, "command not found\n", 18);
-        exit(1);
-    }
+void execute_command(char **cmd, char **cmd2, char **envp) {
+    char *cmd_path;
+
     cmd_path = find_executable_path(cmd[0], envp);
     if (cmd_path) {
         execve(cmd_path, cmd, envp);
+        perror("execve");
         free(cmd_path);
+        ft_free(cmd);
+        ft_free(cmd2);
+        exit(1);
     }
-
-    write(2, "command not found\n", 18);
-    exit(1);
 }
 
-void child_process_1(int *fd, char *input_file, char **cmd, char **envp) {
+void child_process_1(int *fd, char *input_file, char **cmd, char **cmd2, char **envp) {
     int in_file = open(input_file, O_RDONLY | O_CREAT, 0644);
     if (in_file == -1) {
-        write(2, "Error opening input file\n", 26);
+        perror("open");
+        ft_free(cmd);
+        ft_free(cmd2);        
         exit(1);
     }
     dup2(in_file, 0);
@@ -68,13 +59,15 @@ void child_process_1(int *fd, char *input_file, char **cmd, char **envp) {
     close(fd[0]);
     dup2(fd[1], 1);
     close(fd[1]);
-    execute_command(cmd, envp);
+    execute_command(cmd, cmd2, envp);
 }
 
-void child_process_2(int *fd, char *output_file, char **cmd, char **envp) {
+void child_process_2(int *fd, char *output_file, char **cmd2, char **cmd, char **envp) {
     int out_file = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
     if (out_file == -1) {
-        write(2, "Error opening output file\n", 27);
+        perror("open");
+        ft_free(cmd);
+        ft_free(cmd2);        
         exit(1);
     }
     close(fd[1]);
@@ -82,7 +75,7 @@ void child_process_2(int *fd, char *output_file, char **cmd, char **envp) {
     close(fd[0]);
     dup2(out_file, 1);
     close(out_file);
-    execute_command(cmd, envp);
+    execute_command(cmd2, cmd, envp);
 }
 
 int main(int ac, char **av, char **envp) {
@@ -95,20 +88,29 @@ int main(int ac, char **av, char **envp) {
         return 1;
     }
     char **cmd1 = ft_split(av[2], ' ');
+    if (!cmd1)
+        return 1;
     char **cmd2 = ft_split(av[3], ' ');
-    if (!cmd1 || !cmd2) 
-        return (ft_free(cmd1), ft_free(cmd2), 1);
+    if (!cmd2) 
+        return (ft_free(cmd1), 1);
     int fd[2];
     if (pipe(fd) == -1)
+    {
+        perror("pipe");
         return (ft_free(cmd1), ft_free(cmd2), 1);
-    
+    }
     pid_t id1 = fork();
+    if (id1 < 0)
+    {
+        perror("fork");
+        return (ft_free(cmd1), ft_free(cmd2), 1);
+    }
     if (id1 == 0)
-        child_process_1(fd, av[1], cmd1, envp);
+        child_process_1(fd, av[1], cmd1, cmd2, envp);
     
     pid_t id2 = fork();
     if (id2 == 0)
-        child_process_2(fd, av[4], cmd2, envp);
+        child_process_2(fd, av[4], cmd2, cmd1, envp);
     
     close(fd[0]);
     close(fd[1]);
